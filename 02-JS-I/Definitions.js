d4907d00387c0279019c0b76fd3f70a1e26accca
unknown <medinaho21@gmail.com>
2022-06-12 13:23:13 -0500

    First Part of Homework

---
+++ 02-JS-I/Definitions.js
@@ -0,0 +1,11 @@
+//Variables: Son declaraciones (letra, palabra, caracteres, etc.) que guardan un valor asignado
+// que se puede usar en cualquier otro momento.
+
+//Strings: Son cadenas de texto (letras, numeros, caracteres). En el caso de numeros
+//no se pueden hacer operaciones matemáticas con ellos.
+// Funciones: Son instrucciones que ejecuta el computador a partir de unos parametros iniciales
+//  y que se pueden invocar en cualquier momento.
+//Declaraciones (if): Son condiciones o preguntas que se hace el programa y que debe tener
+//una respuesta como SI o NO (Verdadero o Falso) y segun la respuesta que arroje, ejecuta una acción.
+// Valores Booleanos: Son respuestas lógicas a una pregunta que solo puede tener dos opciones
+// verdadero o falso, SI o NO.
